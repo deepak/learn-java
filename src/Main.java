547f7d528fe497877c882df2f653469c1fd334e9
Deepak Kannan <kannan.deepak@gmail.com>
2015-12-21 12:07:47 +0530

    add Person.equals and hashCode for equality check. also use Object.equals to check for null first

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Objects;
+
 public class Main {
 
     public static void main(String[] args) {
         Person p1 = new Person();
         p1.setName("p1");
         System.out.println(p1);
+
+        Person p2 = new Person("p1");
+
+        // Object.equals will check for null
+        System.out.println("person equal null: " + Objects.equals(p1, null));
+        System.out.println("both person equal: " + Objects.equals(p1, p2));
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,16 @@
+import java.util.Objects;
+
 public class Person {
     private String name;
     private int age; // defaults to 0 - if not initialized
     private Integer githubID; // defaults to null - if not initialized
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -24,5 +33,26 @@ public class Person {
 //                ", age=" + age +
 //                ", githubID=" + githubID +
 //                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        if (name != null ? !name.equals(person.name) : person.name != null) return false;
+        return !(githubID != null ? !githubID.equals(person.githubID) : person.githubID != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        result = 31 * result + (githubID != null ? githubID.hashCode() : 0);
+        return result;
     }
 }
